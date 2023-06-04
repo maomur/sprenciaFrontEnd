2fc959adb0ffd9a7ecf912a1bacc8111316b8375
Mauricio Murcia S <maomur@gmail.com>
2023-06-04 13:28:51 +0200

    Refactorización Inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,34 +2,30 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { SobreNosotrosComponent } from './components/sobre-nosotros/sobre-nosotros.component';
-import { CursosComponent } from './components/cursos/cursos.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 import { FormularioCursoComponent } from './components/formulario-curso/formulario-curso.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
-import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { RecuperarClaveComponent } from './components/recuperar-clave/recuperar-clave.component';
 import { CursoComponent } from './components/curso/curso.component';
 import { CrearCursoComponent } from './components/crear-curso/crear-curso.component';
-import { CursosCategoriaComponent } from './components/cursos-categoria/cursos-categoria.component';
+import { CursosCategoriaComponent } from './components/cursos-resultado/cursos-categoria.component';
+import { CursosGridComponent } from './components/cursos-grid/cursos-grid.component';
 
 const routes: Routes = [
-  // { path: "", pathMatch: 'full', redirectTo: '/home' },
   { path: "", component: HomeComponent },
   { path: "sobre-nosotros", component: SobreNosotrosComponent },
-  { path: "cursos", component: CursosComponent },
   { path: "formulario-curso", component: FormularioCursoComponent },
   { path: "usuarios", component: UsuariosComponent },
   { path: "perfil", component: PerfilComponent },
-  { path: "login", component: LoginComponent },
   { path: "registro", component: RegistroComponent },
   { path: "contacto", component: ContactoComponent },
   { path: "recuperar-clave", component: RecuperarClaveComponent },
-  { path: "cursos", component: CursosComponent },
   { path: "curso/:id", component: CursoComponent },
   { path: "crear-curso", component: CrearCursoComponent },
-  { path: "resultado-categoria/:categoria", component: CursosCategoriaComponent }
+  { path: "resultado-categoria/:categoria", component: CursosCategoriaComponent },
+  { path: 'cursos', component: CursosGridComponent }
 ];
 
 @NgModule({
